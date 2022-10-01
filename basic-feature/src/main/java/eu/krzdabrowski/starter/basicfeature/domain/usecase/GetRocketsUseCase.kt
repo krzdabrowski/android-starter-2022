94ac2e77bd86c7c2071bffbfe2bebb098f828f30
Krzysztof Dabrowski <krz.dabrowski95@gmail.com>
2022-10-01 13:19:40 +0200

    Fix bug with cancelling Flow during first app launch with thrown exception

--- basic-feature/src/main/java/eu/krzdabrowski/starter/basicfeature/domain/usecase/GetRocketsUseCase.kt
+++ basic-feature/src/main/java/eu/krzdabrowski/starter/basicfeature/domain/usecase/GetRocketsUseCase.kt
@@ -3,8 +3,14 @@ package eu.krzdabrowski.starter.basicfeature.domain.usecase
 import eu.krzdabrowski.starter.basicfeature.domain.model.Rocket
 import eu.krzdabrowski.starter.basicfeature.domain.repository.RocketRepository
 import eu.krzdabrowski.starter.core.extensions.resultOf
+import java.io.IOException
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.retryWhen
+
+private const val RETRY_TIME_IN_MILLIS = 15_000L
 
 fun interface GetRocketsUseCase : () -> Flow<Result<List<Rocket>>>
 
@@ -14,4 +20,17 @@ fun getRockets(
     .getRockets()
     .map {
         resultOf { it }
+    }
+    .retryWhen { cause, _ ->
+        if (cause is IOException) {
+            emit(Result.failure(cause))
+
+            delay(RETRY_TIME_IN_MILLIS)
+            true
+        } else {
+            false
+        }
+    }
+    .catch { // for other than IOException but it will stop collecting Flow
+        emit(Result.failure(it)) // also catch does re-throw CancellationException
     }
